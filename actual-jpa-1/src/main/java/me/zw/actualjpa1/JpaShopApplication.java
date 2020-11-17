bb792ae8b1dfa63a2901874f63bd296972229e0a
zw <zerowest1001@gmail.com>
2020-11-17 09:26:15 +0900

    Initial Commit

---
+++ actual-jpa-1/src/main/java/me/zw/actualjpa1/JpaShopApplication.java
@@ -0,0 +1,13 @@
+package me.zw.actualjpa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaShopApplication.class, args);
+    }
+
+}
---
+++ actual-jpa-1/src/test/java/me/zw/actualjpa1/JpaShopApplicationTest.java
@@ -0,0 +1,13 @@
+package me.zw.actualjpa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaShopApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
