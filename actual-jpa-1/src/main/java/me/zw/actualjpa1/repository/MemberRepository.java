c518b912d19ab6158d1446e800f8e408c6596e37
zw <zerowest1001@gmail.com>
2020-11-18 00:16:32 +0900

    회원 도메인 개발

---
+++ actual-jpa-1/src/main/java/me/zw/actualjpa1/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package me.zw.actualjpa1.repository;
+
+import me.zw.actualjpa1.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void save(Member member) {
+        entityManager.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return entityManager.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return entityManager.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return entityManager.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
---
+++ actual-jpa-1/src/test/java/me/zw/actualjpa1/service/MemberServiceTest.java
@@ -0,0 +1,51 @@
+package me.zw.actualjpa1.service;
+
+import me.zw.actualjpa1.domain.Member;
+import me.zw.actualjpa1.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    public void 회원가입_테스트() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+    }
+}
