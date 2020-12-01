2f2803f2ab630e129f120384f401b3069463fa06
zw <zerowest1001@gmail.com>
2020-12-01 14:22:28 +0900

    Initial Commit

---
+++ data-jpa/src/test/java/me/zw/datajpa/DataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zw.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
